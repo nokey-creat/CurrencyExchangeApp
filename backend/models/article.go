ef746ed7f6f398460db34f0851a7a6c820c28f9d
啊 <3300247992@qq.com>
2025-09-05 15:22:48 +0800

    feat(controllers): 添加文章作者

--- backend/models/article.go
+++ backend/models/article.go
@@ -5,9 +5,9 @@ import "gorm.io/gorm"
 //文章的模型
 type Article struct {
 	gorm.Model
-	Title   string `binding:"required" json:"Title"`
-	Content string `binding:"required" json:"Content"`
-	Preview string `binding:"required" json:"Preview"`
-	//Likes   int    `gorm:"default:0" json:"Likes"`
-	//Author string `json:"-"`
+	Title   string `binding:"required" json:"title"`
+	Content string `binding:"required" json:"content"`
+	Preview string `binding:"required" json:"preview"`
+	//Likes   int    `gorm:"default:0" json:"Likes"` //点赞通过redis实现了
+	Author string `json:"author"`
 }
